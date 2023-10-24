785e0f1c937a847429b8b9277a7f1b5178dc3b14
coubs <john.mccoubrie15@gmail.com>
2023-10-24 16:14:56 -0400

    Implemented backend logic

--- ChatSample/ChatSample/Hubs/ChatHub.cs
+++ ChatSample/ChatSample/Hubs/ChatHub.cs
@@ -1,14 +1,53 @@
 ﻿using Microsoft.AspNetCore.SignalR;
-using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ChatSample.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task Send(string name, string message)
+        private static List<Message> Messages = new List<Message>();
+
+        public async void SendMessage(string user, string messageText)
         {
-            // Call the broadcastMessage method to update clients.
-            await Clients.All.SendAsync("broadcastMessage", name, message);
+            var message = new Message
+            {
+                Creator = user,
+                Text = messageText
+            };
+
+            Messages.Add(message);
+
+            await Clients.All.SendAsync("broadcastMessage", user, messageText, message.ID);
+        }
+
+        public async void AddReaction(string messageId, string emoji, string user)
+        {
+            var message = Messages.FirstOrDefault(m => m.ID == messageId);
+            if (message != null)
+            {
+                var reaction = message.Reactions.FirstOrDefault(r => r.Emoji == emoji);
+                if (reaction == null)
+                {
+                    reaction = new Reaction
+                    {
+                        Emoji = emoji
+                    };
+                    message.Reactions.Add(reaction);
+                }
+
+                if (reaction.UsersReacted.Contains(user))
+                {
+                    // user has already reacted with this emoji
+                    reaction.UsersReacted.Remove(user);
+                }
+                else
+                {
+                    reaction.UsersReacted.Add(user);
+                }
+
+                await Clients.All.SendAsync("updateReactions", messageId, emoji, reaction.Count);
+            }
         }
     }
 }
---
+++ ChatSample/ChatSample/Message.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace ChatSample
+{
+    public class Message
+    {
+        public string ID { get; set; } = Guid.NewGuid().ToString();
+        public string Creator { get; set; }
+        public string Text { get; set; }
+        public List<Reaction> Reactions { get; set; } = new List<Reaction>();
+    }
+}
+
