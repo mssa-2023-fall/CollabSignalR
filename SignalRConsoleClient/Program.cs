e32b29776361ff26a7d85fef5f14c89a958e8fe3
Alejandro Gonzalez <alejandro.gonzalezde@outlook.com>
2023-10-25 09:36:42 -0400

    Update Program.cs

--- SignalRConsoleClient/Program.cs
+++ SignalRConsoleClient/Program.cs
@@ -1,75 +1,101 @@
-﻿using Microsoft.AspNetCore.SignalR;
-using Microsoft.AspNetCore.SignalR.Client;
-class program
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+Console.WriteLine("Welcome to the NerdCraft Chat");
+Console.WriteLine("Press ESC key to continue");
+Console.CursorVisible = false; // Hide the cursor
+
+int screenWidth = Console.WindowWidth;
+int position = 0;
+string hotdog = "HOTDOG!!!";
+int animationSpeed = 100; // Adjust the speed of animation (in milliseconds)
+bool exitRequested = false;
+
+Thread inputThread = new Thread(() =>
 {
-    static async void Main(string[] args)
-    {   //defualt hub
-        var hubUrl = "https://chatsample20231024085542.azurewebsites.net/chat";
-
-        Console.WriteLine("Which hub do you want to connect to? Press Enter for Default");
-        
-        //chance to connect to a different hub
-        var userHubUrl = Console.ReadLine();
-        var connection = new HubConnectionBuilder()
-                        .WithUrl(string.IsNullOrEmpty(userHubUrl) ? hubUrl : userHubUrl)
-                        .Build();
-
-        try
+    while (true)
+    {
+        if (Console.KeyAvailable)
         {
-            connection.StartAsync().Wait();
-            Console.WriteLine("Connection established.");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error starting the connection: {ex.Message}");
-        }
-        
-        Dictionary<string, string> userDictionary = new Dictionary<string, string>(); //Dictionary to map connectionId with Users
-
-        //receive message
-        connection.On<string, string>("ReceiveMessage", (user, message) =>
-        {
-            Console.WriteLine($"{user}: {message}");
-        });
-
-        //notificaiton of connected user
-        connection.On<string>("UserConnected", (connectionId) =>
-        {
-            Console.WriteLine($"User connected with connectionId: {connectionId}");
-            Console.WriteLine("Enter your username: ");
-            var username = Console.ReadLine();
-            userDictionary[connectionId] = username;
-            Console.WriteLine($"Welcome, {username}");
-
-        });
-
-        //notification of disconnected user
-        connection.On<string>("UserDisconnected", (connectionId) =>
-        {
-            if (userDictionary.ContainsKey(connectionId))
+            var key = Console.ReadKey(intercept: true).Key;
+            if (key == ConsoleKey.Escape)
             {
-                var disconnectedUser = userDictionary[connectionId];
-                Console.WriteLine($"{disconnectedUser} has disconnected.");
-                userDictionary.Remove(connectionId);
-            }
-        });
-
-        //Enter message and send message async
-        while(true)
-        {
-            Console.Write($"Enter Message: ");
-            var message = Console.ReadLine();
-
-            if (userDictionary.ContainsKey(connection.ConnectionId))
-            {
-                var username = userDictionary[connection.ConnectionId];
-                await connection.InvokeAsync("SendMessage", username, message);
+                exitRequested = true;
+                break;
             }
         }
     }
+});
+
+inputThread.Start();
+
+while (!exitRequested)
+{
+    Console.SetCursorPosition(position, Console.WindowHeight / 2);
+    Console.Write(hotdog);
+    Thread.Sleep(animationSpeed);
+    Console.SetCursorPosition(position, Console.WindowHeight / 2);
+    Console.Write(new string(' ', hotdog.Length));
+    position++;
+
+    if (position >= screenWidth)
+        position = 0;
 }
 
-// Hub: https://chatsample20231024085542.azurewebsites.net/chat
-//Collect UserName
+inputThread.Join(); // Wait for the input thread to exit
+Console.Clear();
 
-//Leaving it open, it is commited under Development-bm
+var connection = new HubConnectionBuilder()
+    .WithUrl("https://chatsample20231024085542.azurewebsites.net/chat")
+    .Build();
+
+try
+{
+    await connection.StartAsync();
+    Console.WriteLine("Connection established.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error starting the connection: {ex.Message}");
+    return;
+}
+
+var userDictionary = new Dictionary<string, string>(); // Dictionary to map connections to usernames
+
+connection.On<string>("UserConnected", (connectionId) =>
+{
+    Console.WriteLine($"User connected with connectionId: {connectionId}");
+    // Prompt the user to enter their username here
+    Console.Write("Enter your username: ");
+    var username = Console.ReadLine();
+    //add the username to the dictionary
+    //userDictionary[connectionId] = username;
+    Console.WriteLine($"Welcome, {username}!");
+});
+
+connection.On<string, string>("ReceiveMessage", (user, message) =>
+{
+    Console.WriteLine($"{user}: {message}");
+});
+
+connection.On<string>("UserDisconnected", (connectionId) =>
+{
+    if (userDictionary.ContainsKey(connectionId))
+    {
+        var disconnectedUser = userDictionary[connectionId];
+        Console.WriteLine($"{disconnectedUser} has disconnected.");
+        userDictionary.Remove(connectionId);
+    }
+});
+
+while (true)
+{
+    Console.Write("Your message: ");
+    var message = Console.ReadLine();
+
+    // Send the message with the current user's username
+    if (userDictionary.ContainsKey(connection.ConnectionId))
+    {
+        var username = userDictionary[connection.ConnectionId];
+        await connection.InvokeAsync("SendMessage", username, message);
+    }
+}
