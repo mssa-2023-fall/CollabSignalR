230b87085ec0cca03b8b082dfc4badfd582f70cd
eMantonya <108507538+eMantonya@users.noreply.github.com>
2023-10-25 09:49:42 -0500

    test added

--- ChatSample/ChatSample/Hubs/ChatHub.cs
+++ ChatSample/ChatSample/Hubs/ChatHub.cs
@@ -5,6 +5,8 @@ namespace ChatSample.Hubs
 {
     public class ChatHub : Hub
     {
+        
+        public ChatHub() { }
         public async Task Send(string name, string message)
         {
             // Call the broadcastMessage method to update clients.
