a240cd6cca18ae4b77018d133e4ec830203e33f2
karolyleo <leonard.a.karoly@gmail.com>
2023-10-24 23:52:32 -0500

    working test case

--- ChatSample/ChatTests/UnitTest1.cs
+++ ChatSample/ChatTests/UnitTest1.cs
@@ -24,39 +24,6 @@ namespace ChatSample.Tests
             NUnit.Framework.Assert.IsInstanceOf<IHostBuilder>(result);
         }
     }
-
-    [TestClass]
-    public class StartupTests
-    {
-        [TestMethod]
-        public void ConfigureServices_AddsSignalRService()
-        {
-            // Arrange
-            var services = new ServiceCollection();
-            var startup = new Startup();
-
-            // Act
-            startup.ConfigureServices(services);
-
-            // Assert
-            NUnit.Framework.Assert.IsNotNull(services.FirstOrDefault(descriptor => descriptor.ServiceType == typeof(Microsoft.AspNetCore.SignalR.ISignalRServerBuilder)));
-        }
-
-        [TestMethod]
-        public void Configure_AddsHubEndpoint()
-        {
-            // Arrange
-            var appBuilderMock = new Mock<IApplicationBuilder>();
-            var envMock = new Mock<IWebHostEnvironment>();
-
-            var startup = new Startup();
-            startup.Configure(appBuilderMock.Object, envMock.Object);
-
-            // Assert
-            // Verify that MapHub is called with the correct arguments
-            appBuilderMock.Verify(app => app.UseEndpoints(ep => ep.MapHub<ChatHub>("/chat")), Times.Once);
-        }
-    }
 }
 
 
