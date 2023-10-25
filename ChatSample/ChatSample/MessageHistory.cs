5fee216850786188b1bcf57956f3ff5767f5383d
Chinjila <116740251+Chinjila@users.noreply.github.com>
2023-10-25 10:46:05 -0500

    Fix issues "Non-constant static fields should not be visible"

--- ChatSample/ChatSample/MessageHistory.cs
+++ ChatSample/ChatSample/MessageHistory.cs
@@ -1,10 +1,20 @@
 ﻿using ChatSample.Hubs;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace ChatSample
 {
-    public class MessageHistory
+    internal static class MessageHistory
     {
-        internal static SortedList<Message, Message> messages= new ();
+        internal readonly static SortedList<Message, Message> messages;
+        private static SortedList<Message, Message> _messages = new SortedList<Message, Message>();
+        static MessageHistory()
+        {
+            messages = _messages;
+        }
+        internal static void Add(Message message)
+        {
+            _messages.Add(message,message);
+        }
     }
 }
